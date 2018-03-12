4cb525fd994c7b7bac2a371afdaf4c0b220389a9
Raj Manandhar <rajm174@gmail.com>
2018-03-12 15:55:17 +0000

    moved the if statement before changing the joint value

--- scripts/move_individual_joint.py
+++ scripts/move_individual_joint.py
@@ -6,7 +6,6 @@ import numpy
 
 rospy.init_node('Hello_Sawyer1')
 limb = intera_interface.Limb('right')
-loop = True
 current_pose = limb.endpoint_pose() 
 print (current_pose)
 limb.move_to_neutral()
@@ -16,13 +15,13 @@ print(angles)
 question = "give a value for right_j5 in radians? "
 question2 = "would you like to continue the loop?"
 
-while loop:
+while True:
+    if raw_input(question2) == "n":
+        break
     angles['right_j5'] = eval(raw_input(question))
     print(angles)
     limb.move_to_joint_positions(angles)
     current_pose = limb.endpoint_pose() 
     print(current_pose)
     print(angles)
-    if raw_input(question2) == "n":
-        break
     rospy.sleep(0.5)
