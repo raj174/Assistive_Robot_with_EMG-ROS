8fedb83480c1eeee4dea1e0dd8af61c27007976d
Raj Manandhar <rajm174@gmail.com>
2018-04-05 16:50:43 +0100

    added a publisher and suscriber to read and write arduino value.

---
+++ scripts/arduino_suscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+	print data.data
+
+def listener():
+	rospy.init_node('listener', anonymous=True)
+	rospy.Subscriber("Arduino_value", String, callback)
+	rospy.spin()
+
+if __name__ == '__main__':
+	listener()
